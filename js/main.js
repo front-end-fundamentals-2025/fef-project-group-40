0493f411fb5d4d5f28a3af17449f6bf7df52f478
Mathilda <huma24ey@student.ju.se>
2025-03-13 13:55:05 +0100

    prep for js & cart stuff

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const addToCartButton = document.getElementById("add-to-cart-buttton")
+
+
+addToCartButton.addEventListener("click", function (event) {
+    
+})
+
+// i dunno how to do this haha
+// 1. add item to cart: store array thingy
+// 2. if that thing is stored in the array, smth sees that and javascript creates element (the img+text in the shopping cart)
